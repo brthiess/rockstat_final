1ebe1b5778e8e507af6f5ffb0a4264a07b998eac
Brad <brthiess@ualberta.ca>
2015-08-17 19:58:10 -0600

    logo

---
+++ root/views/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+	<div class="footer-factoid">
+		<a href="/description/">What's happening here?</a>
+	</div>
+	<div class="footer-copyright">
+		© 2015 RockStat
+	</div>
+	<div class="footer-about">
+		<a href="/about/">About</a>
+	</div>
+</div>
--- root/views/landing.php
+++ root/views/landing.php
@@ -1,8 +1,21 @@
 <body>
 <div id="landing-container">
 	<div id="search-container">
-		<input id="search-input" placeholder="Search curling stats..."></input>
-		<div id="search-button"></div>
+		<div id="search-input-container">
+			<input id="search-input" placeholder="Search curling stats..."></input>
+			<div id="search-button"></div>
+		</div>
+		
+		<div class="logo-container">
+			<div class="house-container">
+				<div class="logo-house twelve-foot"></div>
+				<div class="logo-house eight-foot"></div>
+				<div class="logo-house four-foot"></div>
+				<div class="logo-house button"></div>
+			</div>
+			<div class="logo-text"><p class="gray-text">R&nbsp;&nbsp;ck</p>Stat</div>
 	</div>
+	</div>
+
 </div>
 </body>
