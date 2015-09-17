f4e6f00d9900228e3ce8f9898c0b6622543545e7
Brad <brthiess@ualberta.ca>
2015-09-17 09:20:44 -0600

    View changes

--- root/js/main.js
+++ root/js/main.js
@@ -1,16 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-	var screen_type = window.innerWidth > 700 ? "desktop" : "mobile";  //Mobile or desktop background
-	var image_url = "/images/" + screen_type + "/background-" + Math.floor((Math.random() * 17) + 1) + ".jpg";
-	
-	//Preload the background
-	preloadPictures([image_url], function(){
-    	document.getElementById("landing-container").className += "landing-container";
-		document.getElementById("landing-container").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.298039), rgba(0, 0, 0, 0.298039)), url(" + image_url + ")";
-	});
-	
+document.addEventListener("DOMContentLoaded", function(event) { 	
 	loadAutocomplete();
-
 });
 
 function loadAutocomplete() {
@@ -28,27 +17,3 @@ function showSuggestions(suggestions){
 		
 	}
 }
-
-//Used to load the background, in the background!
-var preloadPictures = function(pictureUrls, callback) {
-    var i,
-        j,
-        loaded = 0;
-
-    for (i = 0, j = pictureUrls.length; i < j; i++) {
-        (function (img, src) {
-            img.onload = function () {                               
-                if (++loaded == pictureUrls.length && callback) {
-                    callback();
-                }
-            };
-
-            // Use the following callback methods to debug
-            // in case of an unexpected behavior.
-            img.onerror = function () {};
-            img.onabort = function () {};
-
-            img.src = src;
-        } (new Image(), pictureUrls[i]));
-    }
-};
--- root/views/landing.php
+++ root/views/landing.php
@@ -1,14 +1,13 @@
 <body>
 <div id="landing-container">
 	<div id="search-container">
+		<div class="logo-container">
+			<img src="/images/logo2.png"/>
+		</div>
 		<div id="search-input-container">
 			<input id="search-input" placeholder="Search curling stats..."></input>
 			<ul class="auto-complete"><li>Hello</li><li>test</li></ul>
 			<div id="search-button"></div>
-		</div>
-		
-		<div class="logo-container">
-			<img src="/images/logo2.png"/>
 		</div>
 	</div>
 
